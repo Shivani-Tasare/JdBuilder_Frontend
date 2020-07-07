273c9b5f09547d2bdeaf67ad69d5029ba4c51e62
Srawat <Srawat@DMINBNOI18035>
2020-07-07 18:37:18 +0530

    added chart

--- src/app/shared/component/i-cimscandidate-list/i-cimscandidate-list.component.ts
+++ src/app/shared/component/i-cimscandidate-list/i-cimscandidate-list.component.ts
@@ -1,18 +1,56 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { Router } from '@angular/router';
+import { ChartOptions } from 'chart.js';
 
 @Component({
   selector: 'app-i-cimscandidate-list',
   templateUrl: './i-cimscandidate-list.component.html',
   styleUrls: ['./i-cimscandidate-list.component.scss']
 })
-export class ICIMSCandidateListComponent implements OnInit {
+export class ICIMSCandidateListComponent implements OnInit, OnChanges {
   //@Input() data = [];
   @Input() data = {Total: null, CandidateList: []};
   constructor(private router: Router) { }
 
-  ngOnInit() {
+  public pieChartLabels:string[] = ['Exact Match', 'Partial Match'];
+  public pieChartData:number[] = [0, 0];
+  public pieChartType:string = 'pie';
+ 
+  // events
+  // public chartClicked(e:any):void {
+  //   console.log(e);
+  // }
+ 
+  // public chartHovered(e:any):void {
+  //   console.log(e);
+  // }
+  get showChart() {
+    return ((this.pieChartData[0] || this.pieChartData[1]) ? true: false )
   }
+  ngOnInit() {
+    console.log(this.data);
+  }
+  ngOnChanges() {
+    if(!!this.data) {
+      this.data.CandidateList = !!this.data['ExactMatch'].CandidateList.length ? this.data['ExactMatch'].CandidateList : this.data['PartialMatch'].CandidateList;
+      this.pieChartData = [this.data['ExactMatch'].Total, this.data['PartialMatch'].Total];
+    }
+  }
+  public pieChartOptions: ChartOptions = {
+    responsive: true,
+    legend: {
+      position: 'bottom',
+      onClick: null
+    },
+    plugins: {
+      datalabels: {
+        formatter: (value, ctx) => {
+          const label = ctx.chart.data.labels[ctx.dataIndex];
+          return label;
+        },
+      },
+    },
+  };
   getAddress(address) {
     if(!!address) {
       return  address.map((r)=> {
