f2b17ddc96e78e6e52452e05431d64d0ad8dbc80
Srawat <Srawat@DMINBNOI18035>
2020-07-08 21:10:09 +0530

    u[

--- src/app/shared/component/viewresume-details/viewresume-details.component.ts
+++ src/app/shared/component/viewresume-details/viewresume-details.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, SecurityContext } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SmartServiceService } from 'src/app/services/smart-service.service';
 import { ActivatedRoute } from '@angular/router';
-import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-viewresume-details',
@@ -10,10 +9,8 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 export class ViewresumeDetailsComponent implements OnInit {
   email: String;
-  name: string;
   resumeData = 'Nothing to display!';
-  constructor(private smartService: SmartServiceService,private route: ActivatedRoute,
-    private sanitizer: DomSanitizer) { 
+  constructor(private smartService: SmartServiceService,private route: ActivatedRoute) { 
     this.route.params.subscribe(params => {
       this.email = params['id'];
       console.log(params);
@@ -24,25 +21,7 @@ export class ViewresumeDetailsComponent implements OnInit {
 
   ngOnInit() {
         this.smartService.getResumeDetails(this.email).subscribe(response => {
-        this.name = response.substr(0, response.indexOf('Phone:')).replace('"','');
-        response = response.substr(response.indexOf('Phone:'), response.length);  
-        let tags = {
-
-          'Phone:' : `<h5 style="color:red">PHONE:</h5>`, 
-          'Email:' : '<br/><br/><h5>EMAIL:</h5>',
-          'WORK EXPERIENCE:' : '<br/><br/><h5>WORK EXPERIENCE:</h5>',
-          'PROJECTS:': '<br/><br/><h5>PROJECTS:</h5>',
-          'TECHNICAL SKILLS:' : '<br/><br/><h5>TECHNICAL SKILLS:</h5>',
-          'EDUCATION:': '<br/><br/><h5>EDUCATION:</h5>',
-          'COURSES:' : '<br/><br/><h5>COURSES:</h5>'};
-
-            response = response.replace(/Phone:|Email:|WORK EXPERIENCE:|PROJECTS:|TECHNICAL SKILLS:|EDUCATION:|COURSES:/gi, 
-              (m:any)=>{
-                return tags[m];
-            })
-
-
-        this.resumeData = this.sanitizer.sanitize(SecurityContext.HTML, response)
+            this.resumeData = response;
     })
   }
 
