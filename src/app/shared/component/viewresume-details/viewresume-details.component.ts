e953d0fb9e5fb3aa5e548886ae861b35a53a9d99
Srawat <Srawat@DMINBNOI18035>
2020-07-09 11:20:17 +0530

    update...

--- src/app/shared/component/viewresume-details/viewresume-details.component.ts
+++ src/app/shared/component/viewresume-details/viewresume-details.component.ts
@@ -24,25 +24,25 @@ export class ViewresumeDetailsComponent implements OnInit {
 
   ngOnInit() {
         this.smartService.getResumeDetails(this.email).subscribe(response => {
-        this.name = response.substr(0, response.indexOf('Phone:')).replace('"','');
-        response = response.substr(response.indexOf('Phone:'), response.length);  
-        let tags = {
+        // this.name = response.substr(0, response.indexOf('Phone:')).replace('"','');
+        // response = response.substr(response.indexOf('Phone:'), response.length);  
+        // let tags = {
 
-          'Phone:' : `<h5 style="color:red">PHONE:</h5>`, 
-          'Email:' : '<br/><br/><h5>EMAIL:</h5>',
-          'WORK EXPERIENCE:' : '<br/><br/><h5>WORK EXPERIENCE:</h5>',
-          'PROJECTS:': '<br/><br/><h5>PROJECTS:</h5>',
-          'TECHNICAL SKILLS:' : '<br/><br/><h5>TECHNICAL SKILLS:</h5>',
-          'EDUCATION:': '<br/><br/><h5>EDUCATION:</h5>',
-          'COURSES:' : '<br/><br/><h5>COURSES:</h5>'};
+        //   'Phone:' : `<h5 style="color:red">PHONE:</h5>`, 
+        //   'Email:' : '<br/><br/><h5>EMAIL:</h5>',
+        //   'WORK EXPERIENCE:' : '<br/><br/><h5>WORK EXPERIENCE:</h5>',
+        //   'PROJECTS:': '<br/><br/><h5>PROJECTS:</h5>',
+        //   'TECHNICAL SKILLS:' : '<br/><br/><h5>TECHNICAL SKILLS:</h5>',
+        //   'EDUCATION:': '<br/><br/><h5>EDUCATION:</h5>',
+        //   'COURSES:' : '<br/><br/><h5>COURSES:</h5>'};
 
-            response = response.replace(/Phone:|Email:|WORK EXPERIENCE:|PROJECTS:|TECHNICAL SKILLS:|EDUCATION:|COURSES:/gi, 
-              (m:any)=>{
-                return tags[m];
-            })
+        //     response = response.replace(/Phone:|Email:|WORK EXPERIENCE:|PROJECTS:|TECHNICAL SKILLS:|EDUCATION:|COURSES:/gi, 
+        //       (m:any)=>{
+        //         return tags[m];
+        //     })
 
 
-        this.resumeData = this.sanitizer.sanitize(SecurityContext.HTML, response)
+        this.resumeData = this.sanitizer.sanitize(SecurityContext.HTML, response.replace(/(?:\r\n|\r|\n)/g, '<br>'))
     })
   }
 
