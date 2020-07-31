4cbb3288cfb361dbbdfc0a29f08b024c39ca4ac6
Srawat <Srawat@DMINBNOI18035>
2020-07-31 11:08:06 +0530

    updated...

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AdalService } from '../shared/services/adal.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(private adalService: AdalService) { }
+  logout() {
+    this.adalService.logout();
+  }
 
   ngOnInit() {
   }
