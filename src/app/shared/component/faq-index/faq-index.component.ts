25eb9290dff5b28ecdee02ab86527dff30b73a1c
Shivani Tasare <Stasare@dminc.com>
2020-08-07 00:10:40 +0530

    faq page half-done

--- src/app/shared/component/faq-index/faq-index.component.ts
+++ src/app/shared/component/faq-index/faq-index.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./faq-index.component.scss']
 })
 export class FAQIndexComponent implements OnInit {
-
+  panelOpenState = false;
   constructor() { }
 
   ngOnInit() {
