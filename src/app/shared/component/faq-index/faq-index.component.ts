a71d25cb78d99976f3227c65aa254b4c30e50755
Shivani Tasare <Stasare@dminc.com>
2020-08-07 18:22:31 +0530

    faq updated..

--- src/app/shared/component/faq-index/faq-index.component.ts
+++ src/app/shared/component/faq-index/faq-index.component.ts
@@ -11,22 +11,5 @@ export class FAQIndexComponent implements OnInit {
 
   ngOnInit() {
   }
-  scrolltoDiv(id){
-    id === 'search_anchor' ? document.getElementById('search').scrollIntoView({behavior : "smooth"}) : null ;
-    id === 'designation_anchor' ? document.getElementById('designation').scrollIntoView({behavior : "smooth"}) : null ;
-    id === 'dashboard_anchor' ? document.getElementById('dashboard').scrollIntoView({behavior : "smooth"}) : null ;
-    id === 'experience_anchor' ? document.getElementById('experience').scrollIntoView({behavior : "smooth"}) : null ;
-    id === 'create_anchor' ? document.getElementById('createdby').scrollIntoView({behavior : "smooth"}) : null ;
-    id === 'location_anchor' ? document.getElementById('location').scrollIntoView({behavior : "smooth"}) : null ;
-    id === 'newJD_anchor' ? document.getElementById('newJd').scrollIntoView({behavior : "smooth"}) : null ;
-    id === 'myJd_anchor' ? document.getElementById('myJD').scrollIntoView({behavior : "smooth"}) : null ;
-    id === 'sharedJD_anchor' ? document.getElementById('sharedjd').scrollIntoView({behavior : "smooth"}) : null ;
-    id === 'EditJD_anchor' ? document.getElementById('editJD').scrollIntoView({behavior : "smooth"}) : null ;
-    // id === 'Ipool_anchor' ? document.getElementById('dashboard').scrollIntoView({behavior : "smooth"}) : null ;
-    // id === 'Epool_anchor' ? document.getElementById('dashboard').scrollIntoView({behavior : "smooth"}) : null ;
-     id === 'login_A' ? document.getElementById('login').scrollIntoView({behavior : "smooth"}) : null ;
-    // id === 'dashboard_anchor' ? document.getElementById('dashboard').scrollIntoView({behavior : "smooth"}) : null ;
-    
 
-  }
 }
