66cd6605b5f8a9f26ba2f81c3a3b8fbafa931a6f
Shivani Tasare <Stasare@dminc.com>
2020-08-28 13:30:00 +0530

    faq updated

--- src/app/shared/component/faq-index/faq-index.component.ts
+++ src/app/shared/component/faq-index/faq-index.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-faq-index',
-  templateUrl: './faq-index.component.html',
-  styleUrls: ['./faq-index.component.scss']
+  selector: "app-faq-index",
+  templateUrl: "./faq-index.component.html",
+  styleUrls: ["./faq-index.component.scss"],
 })
 export class FAQIndexComponent implements OnInit {
   panelOpenState = false;
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
